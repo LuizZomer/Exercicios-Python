41af60d4e1abc726f997941a4e33c25740b2e7c4
Luiz Felipe Zomer <129988357+LuizZomer@users.noreply.github.com>
2023-05-19 16:51:00 -0300

    com enunciado

--- listas/exer006.py
+++ listas/exer006.py
@@ -1,3 +1,11 @@
+# Exercício Python 084: Faça um programa que leia nome e peso de várias pessoas,                                    guardando tudo em uma lista. No final mostre:                                                                                                  
+#A)Quantas pessoas foram cadastradas.                                                                                                        B) Uma listagem com as pessoas mais pesadas.                                                                                                    
+# C)listagem com as pessoas mais leves.
+
+
+
+
+
 pessoas = []
 dados = []
 leves = []
